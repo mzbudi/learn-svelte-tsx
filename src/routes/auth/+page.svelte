f1efd98e26d43e6793020c4e5ef45ec62bd23a56
mzbudi <ronibudianto361@gmail.com>
2023-02-01 16:43:39 +0700

    Initial Commit

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col justify-center items-center self-center w-full h-full">
+	<input type="text" placeholder="username" class="p-2 rounded-md border mt-2" />
+	<input type="password" placeholder="password" class="p-2 rounded-md border mt-2" />
+	<button class="bg-blue-500 mt-2 p-2 rounded">Submit</button>
+</div>
